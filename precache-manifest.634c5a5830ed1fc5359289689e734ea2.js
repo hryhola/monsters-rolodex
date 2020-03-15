544fe3b5dfe3a61978fe4899f2a7532209a4de73
Vlad <vladgrygola@gmail.com>
2020-03-15 21:45:38 +0200

    Updates

---
+++ precache-manifest.634c5a5830ed1fc5359289689e734ea2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca21c3002447e4d881226cede9ea48c2",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "758410a333b42e56d48c",
+    "url": "/monsters-rolodex/static/css/main.2b3216e1.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "758410a333b42e56d48c",
+    "url": "/monsters-rolodex/static/js/main.a712d476.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
